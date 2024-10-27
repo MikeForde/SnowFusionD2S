fa147398fd1953e9dc9397c259da112d487c0b9e
MikeForde <mikeforde@ymail.com>
2024-10-27 07:10:17 +0000

    Review Page update 2

--- client/src/pages/ReviewPage.js
+++ client/src/pages/ReviewPage.js
@@ -38,7 +38,7 @@ function ReviewPage() {
                     <p>Two primary systems were established to facilitate this review process:</p>
                     <ol>
                         <li>
-                            <strong>SnowStorm Server</strong>: A local copy of the NHS SNOMED server (SnowStorm) was created within Docker, containing a complete set of International and NHS SNOMED codes (as of Sep 2022).
+                            <strong>SnowStorm Server</strong>: A copy of the NHS SNOMED server system (SnowStorm) was created locally within Docker and seeded with a complete set of International and NHS SNOMED codes (as at Sep 2022).
                         </li>
                         <li>
                             <strong>SnowFusion-Access Application</strong>: A custom-built VBA Access application was created to serve as the single source of truth during the review. This application included:
@@ -65,7 +65,40 @@ function ReviewPage() {
                         <li>
                             Some codes were deliberately not mapped due to functional or structural differences between DMS needs and SNOMED hierarchy, ensuring the DMS-specific context was preserved.
                         </li>
+                        <li>In total 2392 codes were mapped, 1710 by API matches and 682 manually.</li>
                     </ul>
+                </Card.Body>
+            </Card>
+            {/* Inactivation process */}
+            <Card className="mb-4">
+                <Card.Body>
+                    <Card.Title>Inactivation Process</Card.Title>
+                    <p>The selection process for inactivation was also handled manually. However, some broad groups of codes were identified with similar inactivation criteria:</p>
+                    <ul>
+                        <li>
+                            <strong>High Priority Business Critical Codes</strong>: Only 51 codes from this group were earmarked for inactivation. All but two of these describe transitions between grades, such as "from-grade" and "to-grade" transitions. These codes were initially required due to limitations in the system, though ideally, grade transitions would be managed more accurately.
+                        </li>
+                        <li>
+                            <strong>Medium Priority Business Critical Codes</strong>: A total of 233 medium-priority codes were identified for inactivation for various reasons, including:
+                            <ul>
+                                <li><strong>Legacy Codes</strong>: Outdated codes no longer in active use.</li>
+                                <li><strong>Military Occupational Role Codes</strong>: It was determined that a future HR system should be the authoritative source for a patient's occupation, rendering these codes unnecessary. Some of these codes exist in SNOMED but are inconsistently used, and separate discussions are underway to potentially work with the NHS to remove them.</li>
+                                <li><strong>Template-Specific Codes</strong>: Due to DMICP limitations, template controls needed to be linked to codes to save data to consultations. Some codes were created solely for this purpose and have limited meaning outside of these templates, which will no longer exist.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <strong>Low Priority - Codes include due to usage in last 5yrs</strong>: A total of 774 low-priority codes were identified for inactivation. In addition to the above there are other reasons including:
+                            <ul>
+                                <li><strong>Location and Militatry Unit codes</strong>: These will not be used in the future system.</li>
+                                <li><strong>Poor Construction</strong>: Some codes were earmarked for inactivation because they simply could not work alongside SNOMED e.g. overloaded terms</li>
+                                <li><strong>Duplication</strong>:  Some DMS codes served the same purpose as other more established DMS codes.</li>
+                                <li><strong>Low Usage</strong>: While low usage alone was not used to determine that a code should be inactivated, very low usage coupled with a taxonomic meaning issues meant on balance that these codes were then selected.</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <p>
+                        Each code inactivated has a reason comment entered, explaining why it was made inactive. While these comments are currently unavailable in the WebApp, plans are in place to make them accessible in the future.
+                    </p>
                 </Card.Body>
             </Card>
         </Container>
